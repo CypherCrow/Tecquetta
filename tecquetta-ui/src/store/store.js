e711f5f8a9f434fdee5bfa1dfa1758f23c238617
Jonah Morgan <jonah3254@gmail.com>
2021-07-15 00:35:58 -0700

    Created new Vue project for app UI

---
+++ tecquetta-ui/src/store/store.js
@@ -0,0 +1,22 @@
+import { createStore } from 'vuex'
+
+const store = createStore({
+    state: {
+
+    },
+    mutations: {
+
+    },
+    actions: {
+
+    },
+    getters: {
+
+    },
+    modules: {
+
+    }
+})
+
+
+export default store
