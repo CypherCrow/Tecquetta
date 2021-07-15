8ada8989347f23368fb1e7a51816bc19b7f976cf
Jonah Morgan <jonah3254@gmail.com>
2021-07-15 00:40:22 -0700

    Created App.vue and Home.vue
    Starter pages for the Tecquetta application.

--- tecquetta-ui/src/App.vue
+++ tecquetta-ui/src/App.vue
@@ -1,13 +1,17 @@
 <template>
-
+  <div id="app"> 
+    <Home />
+  </div>
 </template>
 
 <script>
 
+import Home from '@/views/Home'
+
 export default {
   name: 'App',
   components: {
-    //HelloWorld
+    Home, 
   }
 }
 </script>
---
+++ tecquetta-ui/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="TecquettaHome">
+        Welcome to Tecquetta! Your app for e-signatures!<br/>
+        This web application uses DocuSign API. 
+    </div> 
+</template>
+
+<script>
+
+export default {
+    name: "Home"
+}
+
+</script>
+
+<style scoped>
+
+.TecquettaHome {
+    text-align: center;
+}
+
+</style>
