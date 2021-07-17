dc47d1930bd538db19c901d298f303756c71c712
Jonah Morgan <jonah3254@gmail.com>
2021-07-16 23:03:00 -0700

    Created Header.vue and updated Home.vue
    Also, updated vuex and vue-router to recent version.

---
+++ src/main/ui/src/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="Header">
+        <header>Tecquetta</header>
+
+        <div class="HeaderLinks"> 
+            <nav> 
+                <li><router-link to="/">Home</router-link></li>
+                <li>Documents</li>
+                <li>Signatures List</li>
+            </nav>
+        </div>
+    </div> 
+</template>
+
+<script>
+
+export default {
+    name: 'Header'
+}
+
+</script>
+
+<style scoped>
+
+.Header {
+    background-color: aqua;
+    display: flex; 
+    margin-bottom: 15px; 
+}
+
+header {
+    margin: 10px 5px; 
+}
+
+.HeaderLinks {
+    padding: 10px 5px;  
+}
+
+li {
+    display: inline; 
+    margin: 10px 5px; 
+} 
+
+a {
+    text-decoration: none;
+    padding: 5px; 
+}
+
+a:hover {
+    background-color: teal;
+}
+
+</style>
--- src/main/ui/src/views/Home.vue
+++ src/main/ui/src/views/Home.vue
@@ -1,14 +1,20 @@
 <template>
     <div class="TecquettaHome">
+        <Header />
+
         Welcome to Tecquetta! Your app for e-signatures!<br/>
         This web application uses DocuSign API. 
     </div> 
 </template>
 
 <script>
+import Header from '@/components/Header'
 
 export default {
-    name: "Home"
+    name: "Home",
+    components: {
+        Header,
+    }
 }
 
 </script>
