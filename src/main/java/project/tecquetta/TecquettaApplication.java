f685f81d490d282243205ee74d2260b73532f97d
Jonah Morgan <jonah3254@gmail.com>
2021-08-11 18:29:57 -0700

    Added rudimentary SpringBoot elements to project
    Added ManController and Application classes

---
+++ src/main/java/project/tecquetta/TecquettaApplication.java
@@ -0,0 +1,13 @@
+package project.tecquetta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecquettaApplication {
+    
+    public static void main(String[] args){
+        SpringApplication.run(TecquettaApplication.class, args); 
+    }
+
+}
---
+++ src/main/java/project/tecquetta/controllers/MainController.java
@@ -0,0 +1,14 @@
+package project.tecquetta.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String index(Model model){
+        model.addAttribute("eventName", "Elen sila lumenn' omentielvo!");
+        return "index"; 
+    }
+}
