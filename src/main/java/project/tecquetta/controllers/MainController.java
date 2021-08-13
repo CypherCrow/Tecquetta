87c179766217780dd734f25b9c15ec53f47af236
Jonah Morgan <jonah3254@gmail.com>
2021-08-13 13:29:36 -0700

    Updated pom.xml and MainController.java

--- src/main/java/project/tecquetta/controllers/MainController.java
+++ src/main/java/project/tecquetta/controllers/MainController.java
@@ -1,7 +1,11 @@
 package project.tecquetta.controllers;
 
+/*
+    This is a test RestContoller class for debugging
+*/
+
 //import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+//import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,14 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/messages/")
 public class MainController {
-    @GetMapping("/message")
-    public String index(Model model){
-        model.addAttribute("eventName", "Elen sila lumenn' omentielvo!");
-        return "index"; 
-    }
 
     @GetMapping("/hello")
     public String hello(){
         return "Elen sila lumenn' omentielvo!";
     }
+
 }
